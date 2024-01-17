89dda45c36256a772195c14da6cdb2376705c2ad
psguazz <p.sguazz@gmail.com>
2024-01-17 11:02:32 +0100

    Scoping scaler

--- lib/fly_scaler/scaler.rb
+++ lib/fly_scaler/scaler.rb
@@ -1,17 +1,19 @@
-class Scaler
-  attr_reader :fly
+module FlyScaler
+  class Scaler
+    attr_reader :fly
 
-  def initialize(fly: nil)
-    @fly = fly || FlyApi.new
-  end
+    def initialize(fly: nil)
+      @fly = fly || FlyApi.new
+    end
 
-  def scaleable?
-    Rails.respond_to?(:env) && (Rails.env.production? || Rails.env.staging?)
-  end
+    def scaleable?
+      Rails.respond_to?(:env) && (Rails.env.production? || Rails.env.staging?)
+    end
 
-  def up
-    if fly.running_machines < fly.total_machines
-      fly.start_machine(fly.get_machine_id)
+    def up
+      if fly.running_machines < fly.total_machines
+        fly.start_machine(fly.get_machine_id)
+      end
     end
   end
 end
